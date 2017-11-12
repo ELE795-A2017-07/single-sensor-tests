fc9d5eac4bc9081551bbdbf36170029c531489ad
Olivier Diotte <olivier@diotte.ca>
2017-11-12 18:43:57 -0500

    pingpong.cpp: reformatting

--- pingpong.cpp
+++ pingpong.cpp
@@ -28,7 +28,7 @@
 #include "lora.h"
 
 ///////////////////////////////////////////////////////////////////
-// CHANGE HERE THE LORA MODE, NODE ADDRESS 
+// CHANGE HERE THE LORA MODE, NODE ADDRESS
 #define LORAMODE  1
 #define node_addr 8
 //////////////////////////////////////////////////////////////////
@@ -62,158 +62,158 @@ int loraMode=LORAMODE;
 
 void setup()
 {
-  int e;
-  
-  // Open serial communications and wait for port to open:
-#if defined __SAMD21G18A__ && not defined ARDUINO_SAMD_FEATHER_M0 
-  SerialUSB.begin(38400);
-#else
-  Serial.begin(38400);  
-#endif 
+	int e;
 
-  // Print a start message
-  PRINT_CSTSTR("%s","Simple LoRa ping-pong with the gateway\n");  
+	// Open serial communications and wait for port to open:
+#if defined __SAMD21G18A__ && not defined ARDUINO_SAMD_FEATHER_M0
+	SerialUSB.begin(38400);
+#else
+	Serial.begin(38400);
+#endif
+
+	// Print a start message
+	PRINT_CSTSTR("%s","Simple LoRa ping-pong with the gateway\n");
 
 #ifdef ARDUINO_AVR_PRO
-  PRINT_CSTSTR("%s","Arduino Pro Mini detected\n");  
+	PRINT_CSTSTR("%s","Arduino Pro Mini detected\n");
 #endif
 #ifdef ARDUINO_AVR_NANO
-  PRINT_CSTSTR("%s","Arduino Nano detected\n");   
+	PRINT_CSTSTR("%s","Arduino Nano detected\n");
 #endif
 #ifdef ARDUINO_AVR_MINI
-  PRINT_CSTSTR("%s","Arduino MINI/Nexus detected\n");  
+	PRINT_CSTSTR("%s","Arduino MINI/Nexus detected\n");
 #endif
 #ifdef ARDUINO_AVR_MEGA2560
-  PRINT_CSTSTR("%s","Arduino Mega2560 detected\n");  
+	PRINT_CSTSTR("%s","Arduino Mega2560 detected\n");
 #endif
 #ifdef ARDUINO_SAM_DUE
-  PRINT_CSTSTR("%s","Arduino Due detected\n");  
+	PRINT_CSTSTR("%s","Arduino Due detected\n");
 #endif
 #ifdef __MK66FX1M0__
-  PRINT_CSTSTR("%s","Teensy36 MK66FX1M0 detected\n");
+	PRINT_CSTSTR("%s","Teensy36 MK66FX1M0 detected\n");
 #endif
 #ifdef __MK64FX512__
-  PRINT_CSTSTR("%s","Teensy35 MK64FX512 detected\n");
+	PRINT_CSTSTR("%s","Teensy35 MK64FX512 detected\n");
 #endif
 #ifdef __MK20DX256__
-  PRINT_CSTSTR("%s","Teensy31/32 MK20DX256 detected\n");
+	PRINT_CSTSTR("%s","Teensy31/32 MK20DX256 detected\n");
 #endif
 #ifdef __MKL26Z64__
-  PRINT_CSTSTR("%s","TeensyLC MKL26Z64 detected\n");
+	PRINT_CSTSTR("%s","TeensyLC MKL26Z64 detected\n");
 #endif
-#ifdef ARDUINO_SAMD_ZERO 
-  PRINT_CSTSTR("%s","Arduino M0/Zero detected\n");
+#ifdef ARDUINO_SAMD_ZERO
+	PRINT_CSTSTR("%s","Arduino M0/Zero detected\n");
 #endif
-#ifdef ARDUINO_AVR_FEATHER32U4 
-  PRINT_CSTSTR("%s","Adafruit Feather32U4 detected\n"); 
+#ifdef ARDUINO_AVR_FEATHER32U4
+	PRINT_CSTSTR("%s","Adafruit Feather32U4 detected\n");
 #endif
 #ifdef  ARDUINO_SAMD_FEATHER_M0
-  PRINT_CSTSTR("%s","Adafruit FeatherM0 detected\n");
+	PRINT_CSTSTR("%s","Adafruit FeatherM0 detected\n");
 #endif
 
 // See http://www.nongnu.org/avr-libc/user-manual/using_tools.html
 // for the list of define from the AVR compiler
 
 #ifdef __AVR_ATmega328P__
-  PRINT_CSTSTR("%s","ATmega328P detected\n");
-#endif 
+	PRINT_CSTSTR("%s","ATmega328P detected\n");
+#endif
 #ifdef __AVR_ATmega32U4__
-  PRINT_CSTSTR("%s","ATmega32U4 detected\n");
-#endif 
+	PRINT_CSTSTR("%s","ATmega32U4 detected\n");
+#endif
 #ifdef __AVR_ATmega2560__
-  PRINT_CSTSTR("%s","ATmega2560 detected\n");
-#endif 
-#ifdef __SAMD21G18A__ 
-  PRINT_CSTSTR("%s","ATSAMD21G18A detected\n");
+	PRINT_CSTSTR("%s","ATmega2560 detected\n");
 #endif
-#ifdef __SAM3X8E__ 
-  PRINT_CSTSTR("%s","SAM3X8E ARM Cortex-M3 detected\n");
+#ifdef __SAMD21G18A__
+	PRINT_CSTSTR("%s","ATSAMD21G18A detected\n");
+#endif
+#ifdef __SAM3X8E__
+	PRINT_CSTSTR("%s","SAM3X8E ARM Cortex-M3 detected\n");
 #endif
 
-  // Power ON the module
-  sx1272.ON();
-  
-  // Set transmission mode and print the result
-  e = sx1272.setMode(loraMode);
-  PRINT_CSTSTR("%s","Setting Mode: state ");
-  PRINT_VALUE("%d", e);
-  PRINTLN;
+	// Power ON the module
+	sx1272.ON();
 
-  // enable carrier sense
-  sx1272._enableCarrierSense=true;
-    
-  // Select frequency channel
-  e = sx1272.setChannel(DEFAULT_CHANNEL);
-  PRINT_CSTSTR("%s","Setting Channel: state ");
-  PRINT_VALUE("%d", e);
-  PRINTLN;
-  
-  // Select amplifier line; PABOOST or RFO
+	// Set transmission mode and print the result
+	e = sx1272.setMode(loraMode);
+	PRINT_CSTSTR("%s","Setting Mode: state ");
+	PRINT_VALUE("%d", e);
+	PRINTLN;
+
+	// enable carrier sense
+	sx1272._enableCarrierSense=true;
+
+	// Select frequency channel
+	e = sx1272.setChannel(DEFAULT_CHANNEL);
+	PRINT_CSTSTR("%s","Setting Channel: state ");
+	PRINT_VALUE("%d", e);
+	PRINTLN;
+
+	// Select amplifier line; PABOOST or RFO
 #ifdef PABOOST
-  sx1272._needPABOOST=true;
-  // previous way for setting output power
-  // powerLevel='x';
+	sx1272._needPABOOST=true;
+	// previous way for setting output power
+	// powerLevel='x';
 #else
-  // previous way for setting output power
-  // powerLevel='M';  
+	// previous way for setting output power
+	// powerLevel='M';
 #endif
 
-  // previous way for setting output power
-  // e = sx1272.setPower(powerLevel); 
+	// previous way for setting output power
+	// e = sx1272.setPower(powerLevel);
 
-  e = sx1272.setPowerDBM((uint8_t)MAX_DBM); 
-  PRINT_CSTSTR("%s","Setting Power: state ");
-  PRINT_VALUE("%d", e);
-  PRINTLN;
-  
-  // Set the node address and print the result
-  e = sx1272.setNodeAddress(node_addr);
-  PRINT_CSTSTR("%s","Setting node addr: state ");
-  PRINT_VALUE("%d", e);
-  PRINTLN;
-  
-  // Print a success message
-  PRINT_CSTSTR("%s","SX1272 successfully configured\n");
+	e = sx1272.setPowerDBM((uint8_t)MAX_DBM);
+	PRINT_CSTSTR("%s","Setting Power: state ");
+	PRINT_VALUE("%d", e);
+	PRINTLN;
 
-  delay(500);
+	// Set the node address and print the result
+	e = sx1272.setNodeAddress(node_addr);
+	PRINT_CSTSTR("%s","Setting node addr: state ");
+	PRINT_VALUE("%d", e);
+	PRINTLN;
+
+	// Print a success message
+	PRINT_CSTSTR("%s","SX1272 successfully configured\n");
+
+	delay(500);
 }
 
 
 void loop(void)
 {
-  uint8_t r_size;
-  int e;
+	uint8_t r_size;
+	int e;
 
-  sx1272.CarrierSense();
+	sx1272.CarrierSense();
 
-  sx1272.setPacketType(PKT_TYPE_DATA);
+	sx1272.setPacketType(PKT_TYPE_DATA);
 
-  r_size=sprintf((char*)message, "Ping");
-      
-  while (1) {
+	r_size=sprintf((char*)message, "Ping");
 
-      PRINT_CSTSTR("%s","Sending Ping");  
-      PRINTLN;
-            
-      e = sx1272.sendPacketTimeoutACK(DEFAULT_DEST_ADDR, message, r_size);
+	while (1) {
 
-      // this is the no-ack version
-      // e = sx1272.sendPacketTimeout(DEFAULT_DEST_ADDR, message, r_size);
-            
-      PRINT_CSTSTR("%s","Packet sent, state ");
-      PRINT_VALUE("%d", e);
-      PRINTLN;
-      
-      if (e==3)
-          PRINT_CSTSTR("%s","No Pong!");
-        
-      if (e==0)
-          PRINT_CSTSTR("%s","Pong received from gateway!");      
+			PRINT_CSTSTR("%s","Sending Ping");
+			PRINTLN;
 
-      PRINTLN;
-      
-      delay(10000);    
-  }          
+			e = sx1272.sendPacketTimeoutACK(DEFAULT_DEST_ADDR, message, r_size);
+
+			// this is the no-ack version
+			// e = sx1272.sendPacketTimeout(DEFAULT_DEST_ADDR, message, r_size);
+
+			PRINT_CSTSTR("%s","Packet sent, state ");
+			PRINT_VALUE("%d", e);
+			PRINTLN;
+
+			if (e==3)
+					PRINT_CSTSTR("%s","No Pong!");
+
+			if (e==0)
+					PRINT_CSTSTR("%s","Pong received from gateway!");
+
+			PRINTLN;
+
+			delay(10000);
+	}
 }
 
 int main(void) {
