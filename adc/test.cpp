93774eb3688ba2c768cc12b40ee693d2c4cf1596
Olivier Diotte <olivier@diotte.ca>
2017-11-27 01:39:00 -0500

    adc/test.cpp: chip disable the LoRa module
    since it shares the SPI lines

--- adc/test.cpp
+++ adc/test.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 int main(void) {
 	Adc _adc = Adc(ADC_CS_PIN, ADC_DIN_PIN, ADC_DOUT_PIN, ADC_CLK_PIN, true);
+	digitalWrite(10, HIGH);
 
 	int16_t data = _adc.read(true, 0);
 	cout << "data = " << hex << data << endl;
