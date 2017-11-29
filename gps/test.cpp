26c0e1c58bc5318a520c86c4a3869f1b747b5bea
Olivier Diotte <olivier@diotte.ca>
2017-11-29 06:24:00 -0500

    gps/test.cpp: we are now able to read data from the UART

--- gps/test.cpp
+++ gps/test.cpp
@@ -1,8 +1,24 @@
+#include <iostream>
+
 #include "arduPi.h"
+#include "TinyGPS.h"
+
+using namespace std;
 
 int main(void) {
+	bool valid = false;
 	SerialPi uart;
+	TinyGPS gps;
 	uart.begin(9600);
+
+	while (!valid) {
+		if (uart.available() > 0) {
+			char c = uart.read();
+			valid = gps.encode(c);
+		} else {
+			usleep(50);
+		}
+	}
 
 	return 0;
 }
