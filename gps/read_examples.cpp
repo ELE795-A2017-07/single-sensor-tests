1377a4834ab266b999f001a831fe231b98d5488f
Olivier Diotte <olivier@diotte.ca>
2017-12-13 16:14:05 -0500

    gps: read_examples can also read from log of GPS module
    For some reason, the GPS module is unable to acquire the signal from where I am testing my module.
    But, it happily does so from my window, so I recorded the data on my laptop using a PL2303

--- gps/read_examples.cpp
+++ gps/read_examples.cpp
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-const string EXAMPLES_FILE_STR = "examples.txt";
+const string EXAMPLES_FILE_STR = "data/examples.txt";
+const string GPS_LOG_FILE_STR = "data/gps_cmds.log";
 
 char read_character(ifstream& infile) {
 	char c;
@@ -21,12 +22,26 @@ char read_character(ifstream& infile) {
 int main(void) {
 	bool valid = false;
 	TinyGPS gps;
-	ifstream infile(EXAMPLES_FILE_STR);
+	//ifstream infile(EXAMPLES_FILE_STR);
+	ifstream infile(GPS_LOG_FILE_STR);
 
+	int line = 0;
+	int col = 0;
 	while (!infile.eof()) {
 		char c = read_character(infile);
 		valid = gps.encode(c);
-		cout << "read character " << hex << int(c) << " (" << c << ") valid?" << valid << endl;
+
+		if (valid) {
+			//cout << "read character " << hex << int(c) << " (" << c << ") valid?" << valid << endl;
+			cout << "valid sentence at line " << line << " column " << col << endl;
+		}
+
+		if (c == '\n') {
+			col = 0;
+			line++;
+		} else {
+			col++;
+		}
 	}
 
 	infile.close();
