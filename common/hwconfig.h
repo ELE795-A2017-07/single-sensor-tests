5b00b5431abe640023e8804eed5b3fcf6c4011fd
Olivier Diotte <olivier@diotte.ca>
2017-12-14 09:35:13 -0500

    Add common/hwconfig.h

---
+++ common/hwconfig.h
@@ -0,0 +1,12 @@
+#ifndef HWCONFIG_H
+#define HWCONFIG_H
+
+
+const int ADC_CS_PIN   = 22; //BCM 6
+const int ADC_CLK_PIN  =  0; //BCM 17
+const int ADC_DOUT_PIN =  2; //BCM 27
+const int ADC_DIN_PIN  =  3; //BCM 22
+
+const int DHT_PIN = 8; //BCM GPIO 3
+
+#endif /* Guard */
