acfaffea4886befccb31b0bfcaa212009dc7c71f
Olivier Diotte <olivier@diotte.ca>
2017-12-18 07:14:37 -0500

    soil_rh: use simpler adc.read() interface

--- soil_rh/soil_rh_test.cpp
+++ soil_rh/soil_rh_test.cpp
@@ -13,9 +13,8 @@ int main(void) {
 	Adc _adc = Adc(ADC_CS_PIN, ADC_DIN_PIN, ADC_DOUT_PIN, ADC_CLK_PIN, true);
 
 	for (int j = 0; j < 50; j++) {
-		int16_t out = _adc.read(true, SOIL_RH_CH);
-		float val = out * (5/1024.);
-		cout << "Soil RH = " << hex << out << " which is about " << dec << val << "V" << endl;
+		float val = _adc.read(true, SOIL_RH_CH);
+		cout << "Soil RH = " << dec << val << "V" << endl;
 		Soil_Rh::soil_rh_t rh_type = Soil_Rh::rh_from_voltage(val);
 
 		switch (rh_type) {
